e61d02e6b0cdffa456f775f6251760ad307c1877
carlos-paezf <cpaezferreira@gmail.com>
2022-11-29 23:55:19 -0500

    Initial Proyect

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+// import SingleResponsibilityPrincipleOriginal from './SingleResponsibilityPrinciple/Original/index'
+// import SingleResponsibilityPrincipleCorrected from './SingleResponsibilityPrinciple/Corrected/index'
+// import OpenClosedPrincipleOriginal from './OpenClosedPrinciple/Original/index'
+// import { TitleWithButton, TitleWithLink } from './OpenClosedPrinciple/Corrected'
+// import { BadImplementation } from './LiskovSubstitutionPrinciple/bad-implementation'
+// import { BestPractice } from './LiskovSubstitutionPrinciple/best-practice'
+// import Post from "./InterfaceSegregationPrinciple/Original"
+import Post from "./InterfaceSegregationPrinciple/Corrected"
+
+
+
+function App () {
+    return (
+        <>
+            {/* Principio de responsabilidad única */ }
+            {/* <SingleResponsibilityPrincipleOriginal /> */ }
+            {/* <SingleResponsibilityPrincipleCorrected /> */ }
+
+            {/* Principio de abierto cerrado */ }
+            {/* <OpenClosedPrincipleOriginal title='Prueba' type='withNormalButton' buttonText='Botón' /> */ }
+            {/* <TitleWithButton title='Botón' buttonText='Botón' onClick={() => null} /> */ }
+            {/* <TitleWithLink title='Link' buttonText='Link' href='/' /> */ }
+
+            {/* Principio de sustitución de Liskov */ }
+            {/* <BadImplementation /> */ }
+            {/* <BestPractice /> */ }
+
+            {/* Principio de segregación de interfaz */ }
+            <Post post={ {
+                author: {
+                    name: "Carlos Páez",
+                    age: 21
+                },
+                title: "Prueba de Post",
+                createdAt: new Date()
+            } }></Post>
+        </>
+    )
+}
+
+export default App
